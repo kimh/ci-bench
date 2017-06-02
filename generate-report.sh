909cfffc9c3e985535f52482758141cddb18666b
Kim, Hirokuni <yangkookkim@gmail.com>
2017-06-02 17:25:46 +0900

    wip

---
+++ generate-report.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -ex
+
+s3_bucket="s3://ci-bench2"
+data_dir="./data"
+
+function download_data() {
+    local platform=$1
+    local s3_path="$s3_bucket/circleci/$platform/"
+
+    aws s3 cp $s3_path . --recursive
+}
+
+function main() {
+    local platform=$1
+    local dir=$data_dir/$platform
+
+    rm -rf $dir
+    mkdir -p $dir
+    download_data $platform
+
+    pushd $dir
+    download_data $platform
+    popd
+}
+
+main $1
